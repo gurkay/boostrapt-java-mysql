1c8305b5cf5915b4dfc3d0eeeed4952c5599ae11
gurkay <g_basyigit@hotmail.com>
2023-05-12 08:05:12 +0300

    first commit

---
+++ backend/src/test/java/com/abonedevre/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abonedevre.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ frontend/src/main/java/com/abonedevre/frontend/MainController.java
@@ -0,0 +1,16 @@
+package com.abonedevre.frontend;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String home(){
+        
+        return "home";
+    }
+
+}
