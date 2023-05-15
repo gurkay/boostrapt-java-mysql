44dd26c60e73ed8ac2ef3a041e4e8d994b41f288
Gürkay Başyiğit <g_basyigit@hotmail.com>
2023-05-15 16:32:45 +0300

    added new codes

--- backend/src/main/resources/static/js/modal_dialog.js
+++ backend/src/main/resources/static/js/modal_dialog.js
@@ -5,6 +5,21 @@ function showDeleteConfirmModal(link, entityName) {
     $("#modalBody").text("Are you sure you want to delete this " + entityName +" ID " + entityId + "?");
     $("#modalDialog").modal("show");
   }
-  function clearFilter() {
+
+function clearFilter() {
     window.location = moduleURL;
-  }
+}
+
+function showModalDialog(title, message) {
+    $("#modalTitle").text(title);
+    $("#modalBody").text(message);
+    $("#modalDialog").modal("show");
+}
+
+function showErrorModal(message) {
+    showModalDialog("Error", message);
+}
+
+function showWarningModal(message) {
+    showModalDialog("Warning", message);
+}
